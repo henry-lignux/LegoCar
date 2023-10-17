946576bbcfe9ffed53bcabb52eb96cdf7d0a4688
henry-lignux <stevenshenry2017@gmail.com>
2023-10-17 15:31:05 -0700

    added touch sensor code

---
+++ touch_sensor.m
@@ -0,0 +1,9 @@
+% touch sensor stuff
+brick = ConnectBrick('EV3F');
+display('Push the button.')
+while brick.TouchPressed(1) == 0
+ brick.playTone(100, 300, 500);
+ pause(0.75);
+end
+display('Done!')
+
