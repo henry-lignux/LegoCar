618ac924eb391fe6f65d5181f0df1eeffc0905b2
henry-lignux <stevenshenry2017@gmail.com>
2023-10-17 15:18:06 -0700

    variable for angle and weight

--- sonar_sensor.m
+++ sonar_sensor.m
@@ -2,6 +2,12 @@
 distance = brick.UltrasonicDist(1);
 % Minimum distance from wall before the car turns
 min_distance = 15;
+% Speed of the car
+speed = 40;
+% Weight coefficient
+w_coefficient = 1.05
+% Turn angle
+angle = 100;
 disp(distance);
 
 while 1
@@ -10,17 +16,16 @@ while 1
     % When the car is further than min_distance, it moves forward
     while distance >= min_distance 
         distance = brick.UltrasonicDist(1);
-        brick.MoveMotor('A', -42);
-        brick.MoveMotor('B', -40);
+        brick.MoveMotor('A', -1 * speed * w_coefficient);
+        brick.MoveMotor('B', -1 * speed);
     end
-    % When the car is less far than min_distance, it turns
+    % When the car is less far than min_distance
     while distance < min_distance
         distance = brick.UltrasonicDist(1);
-        brick.MoveMotor('A', -100);
-
+	% it turns right
+        brick.MoveMotor('A', -1 * angle);
+	% and then stops
         brick.StopMotor('AB');
-        
-        
     end
 end
         
