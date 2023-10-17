dc44469d8f513088060ea528618b3a8f2e60e425
nmhom1 <147112049+nmhom1@users.noreply.github.com>
2023-10-17 16:45:17 -0700

    Update light_sensor.m

--- light_sensor.m
+++ light_sensor.m
@@ -1,5 +1,13 @@
 brick.SetColorMode (SensorPort, mode);
 
 %returns a number that is related to the brightness of light that the sensor recieves
-brick.SetColorMode(2, 0);
-brightness = brick.LightReflect(2);
+brick.SetColorMode(3, 2);
+color = brick.ColorCode(3); %read the color code value from sensor on port 3
+
+while 1
+  if color == 5 % if detected red
+  brick.MoveMotor('AB', 0); % break
+  pause(3);
+  brick.MoveMotor('AB', 50); %move
+  else
+
