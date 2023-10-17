af6f8e2e0ca68bc56fe2bd67ebfc55903c697a59
nmhom1 <147112049+nmhom1@users.noreply.github.com>
2023-10-17 16:18:35 -0700

    Update light_sensor.m

--- light_sensor.m
+++ light_sensor.m
@@ -1,1 +1,5 @@
+brick.SetColorMode (SensorPort, mode);
 
+%returns a number that is related to the brightness of light that the sensor recieves
+brick.SetColorMode(2, 0);
+brightness = brick.LightReflect(2);
