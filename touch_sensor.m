2d28a3376ba961b3a24ec05897231224aa0c8fab
henry-lignux <stevenshenry2017@gmail.com>
2023-10-17 15:47:36 -0700

    Added code for turning

--- touch_sensor.m
+++ touch_sensor.m
@@ -1,9 +1,28 @@
 % touch sensor stuff
 brick = ConnectBrick('EV3F');
-display('Push the button.')
-while brick.TouchPressed(1) == 0
- brick.playTone(100, 300, 500);
- pause(0.75);
-end
-display('Done!')
+% % Actual distance from wall
+% distance = brick.UltrasonicDist(1);
+% % Minimum distance from wall before the car turns
+% min_distance = 15;
+% % Speed of the car
+speed = 40;
+% Weight coefficient
+w_coefficient = 1.05
+% Turn angle
+angle = 100;
+% disp(distance);
 
+while 1 
+    % When the car is not touching the wall, it moves forward
+    while brick.TouchPressed(2) == 0 
+        brick.MoveMotor('A', -1 * speed * w_coefficient);
+        brick.MoveMotor('B', -1 * speed);
+    end
+    % When the car is touching the wall
+    while brick.TouchPressed(2) == 1 
+	% it turns right
+        brick.MoveMotor('A', -1 * angle);
+	% and then stops
+        brick.StopMotor('AB');
+    end
+end
