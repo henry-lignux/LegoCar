773f879b76a1ed9b877a74781555bcdbbe3bddff
jfox80 <143219082+jfox80@users.noreply.github.com>
2023-10-18 20:35:59 -0700

    Add files via upload

--- touch_sensor.m
+++ touch_sensor.m
@@ -1,5 +1,4 @@
 % touch sensor stuff
-brick = ConnectBrick('EV3F');
 % % Actual distance from wall
 % distance = brick.UltrasonicDist(1);
 % % Minimum distance from wall before the car turns
@@ -7,22 +6,32 @@ brick = ConnectBrick('EV3F');
 % % Speed of the car
 speed = 40;
 % Weight coefficient
-w_coefficient = 1.05
+w_coefficient = 1.05;
 % Turn angle
 angle = 100;
 % disp(distance);
 
 while 1 
-    % When the car is not touching the wall, it moves forward
-    while brick.TouchPressed(2) == 0 
+    touch = brick.TouchPressed(2);
+    % pause(0.2);
+    display("In First ");
+    disp(touch);% When the car is not touching the wall, it moves forward
+    while touch == 0 
         brick.MoveMotor('A', -1 * speed * w_coefficient);
         brick.MoveMotor('B', -1 * speed);
+        touch = brick.TouchPressed(2);
+        % pause(0.2);
+        disp(brick.TouchPressed(2));
     end
-    % When the car is touching the wall
-    while brick.TouchPressed(2) == 1 
-	% it turns right
-        brick.MoveMotor('A', -1 * angle);
-	% and then stops
+    if touch == 1
+        brick.MoveMotor('AB', 50);
+        pause(0.3);
         brick.StopMotor('AB');
+        pause(3);
+        brick.MoveMotor('A', -50);
+        pause(0.1);        
     end
+
+    % When the car is touching the wall
+    
 end
