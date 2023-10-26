758670e6f78e19f16d6117c51eb7ca429d4cd75a
Andy Le <136204727+4ndy1e@users.noreply.github.com>
2023-10-26 16:45:35 -0700

    Update and rename AndyTest.m to AndySonic.m

---
+++ AndySonic.m
@@ -0,0 +1,22 @@
+distance = brick.UltrasonicDist(1);
+
+while 1 
+  distance = brick.UltrasonicDist(1);
+  display(distance);
+
+  if (distance > 15)
+    brick.MoveMotor('B', -50);
+    display(distance);
+  else if (distance < 15)
+    brick.MoveMotor('AB', -50);
+    display(distance);
+end
+
+  
+    
+    
+    
+    
+    
+
+  
