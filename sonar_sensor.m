e16cbb2dc7c77a0178e2a59afcf9e2e16c531c9c
jfox80 <143219082+jfox80@users.noreply.github.com>
2023-10-26 16:08:04 -0700

    Update sonar_sensor.m

--- sonar_sensor.m
+++ sonar_sensor.m
@@ -1,38 +1,34 @@
-% Actual distance from wall
-distance = brick.UltrasonicDist(1);
-% Minimum distance from wall before the car turns
-min_distance = 15;
-% Speed of the car
-speed = 40;
-% Weight coefficient
-w_coefficient = 1.05
-% Turn angle
-angle = 100;
-disp(distance);
+        distance = brick.UltrasonicDist(1);
+        touch = brick.TouchPressed(2);
+        while 1
+            brick.StopMotor('AB');
+            distance = brick.UltrasonicDist(1);
+            touch = brick.TouchPressed(2);
+            disp(distance);
+            if (touch == 1)
+                   brick.StopMotor('B');
+                   brick.MoveMotor('A', -30);
+                   pause(0.4);
+                   touch = brick.TouchPressed(2);
+                   disp(touch);
+            else
+                while distance < 10
+                    brick.MoveMotor('A', -50);
+                    brick.MoveMotor('B', -51);
+                    distance = brick.UltrasonicDist(1);
+                    disp(distance);
+                    touch = brick.TouchPressed(2);
+                    break;
+                end
+                brick.StopMotor('AB');
+            
+                brick.MoveMotor('B', -60);
 
-while 1
-    distance = brick.UltrasonicDist(1);
-    disp(distance);
-    % When the car is further than min_distance, it moves forward
-    while distance >= min_distance 
-        distance = brick.UltrasonicDist(1);
-	if brick.TouchPressed(2)
- 		brick.MoveMotor('A', -1 * speed * w_coefficient);
-        	brick.MoveMotor('B', -1 * (speed + 10));
-	else
-        	brick.MoveMotor('A', -1 * speed * w_coefficient);
-        	brick.MoveMotor('B', -1 * speed);
-	end
-    end
-    % When the car is less far than min_distance
-    while distance < min_distance
-        distance = brick.UltrasonicDist(1);
-	% it turns right
-        brick.MoveMotor('A', -1 * angle);
-	% and then stops
-        brick.StopMotor('AB');
-    end
-end
+                distance = brick.UltrasonicDist(1);
+                disp(distance);
+            end
+           
+        end
         
        
 
