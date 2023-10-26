0a9b3f5acd0df6328bf4baa10e574a31308a0c85
Andy Le <136204727+4ndy1e@users.noreply.github.com>
2023-10-26 16:58:03 -0700

    Update AndyTouch.m

--- AndyTouch.m
+++ AndyTouch.m
@@ -1,1 +1,14 @@
+touch = brick.TouchPressed(2);
 
+while 1 
+  touch = brick.TouchPressed(2);
+  if (touch == 1)
+    brick.stopMotor('AB', 'Brake');
+    brick.MoveMotorAngleRel('AB', 60, -120, 'Brake');
+    brick.WaitForMotor('AB');
+    brick.MoveMotorAngleRel('A', -60, -415, 'Brake');
+    brick.WaitForMotor('A');
+  elseif (touch == 0)
+    brick.MoveMotor('AB', -50);
+  end
+end
