cb7fb2f6c5725626f6da8a3be28d0ec9e63b8a38
Andy Le <136204727+4ndy1e@users.noreply.github.com>
2023-10-26 17:25:42 -0700

    Update AndySonic.m

--- AndySonic.m
+++ AndySonic.m
@@ -4,12 +4,17 @@ while 1
   distance = brick.UltrasonicDist(1);
   display(distance);
 
-  if (distance > 15)
-    brick.MoveMotor('B', -50);
+  if (distance > 30)
+    brick.stopMotor('AB', 'Coast');
+    brick.MoveMotorAngleRel('B', -50, -415, 'Brake');
+    brick.MoveMotor('AB', -60);
+    pause(1);
     display(distance);
-  elseif (distance < 15)
+  elseif (distance < 10)
     brick.MoveMotor('AB', -50);
     display(distance);
+  else
+    brick.MoveMotor('B')
   end
 end
 
